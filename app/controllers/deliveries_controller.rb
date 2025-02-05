aac5f8ce95a1ebbe80de38453ddb787e841988b6
Lance Nelson <lancen04@gmail.com>
2025-02-05 17:12:02 -0500

    started messing around with trying to get my view to properly function

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,8 +1,26 @@
+# class DeliveriesController < ApplicationController
+#   def index
+#     locations = params[:json]
+#     if locations
+#       @stops = MapquestFacade.route_locations(locations)
+#     end
+#   end
+# end
 class DeliveriesController < ApplicationController
   def index
-    locations = params[:json]
-    if locations
+    # Render the form to input locations
+    @stops = []
+  end
+
+  def create
+    locations = params[:locations]
+    if locations.present?
       @stops = MapquestFacade.route_locations(locations)
+      # Optionally, redirect or render a different view after the API call
+      render :index
+    else
+      flash[:alert] = "Please enter locations."
+      render :index
     end
   end
 end
