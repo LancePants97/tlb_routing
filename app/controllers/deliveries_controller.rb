ea2b1132587e2c3299dca8d8f7f82c39d3fa186d
Lance Nelson <lancen04@gmail.com>
2025-02-18 14:41:26 -0500

    added styling and JS scripting

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,9 +1,11 @@
 class DeliveriesController < ApplicationController
   def index
     @stops = []
+    @accounts = Location.all
   end
 
   def create
+    @accounts = Location.all
     locations = params[:locations]
     if locations.present?
       @stops = MapquestFacade.route_locations(locations)
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
 class Location < ApplicationRecord
-
+  def self.alphabetical
+    order(:name)
+  end
 end
